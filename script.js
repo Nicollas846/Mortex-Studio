43c43037fdf1993d67e14afbccb2a28bc7c93fad
Nicollas846 <gggerson1769@gmail.com>
2026-06-28 01:11:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function irInicio() {
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("chatPage").style.display = "block";
+}
+
+function voltarMenu() {
+  document.getElementById("chatPage").style.display = "none";
+  document.getElementById("menu").style.display = "block";
+}
+
 function enviar() {
   let input = document.getElementById("pergunta");
   let mensagens = document.getElementById("mensagens");
@@ -5,19 +15,9 @@ function enviar() {
   let texto = input.value;
   if (!texto) return;
 
-  // mostra pergunta do usuário
   mensagens.innerHTML += "<p><b>Você:</b> " + texto + "</p>";
 
-  // resposta simples (IA fake por enquanto)
-  let resposta = "";
-
-  if (texto.toLowerCase().includes("combo")) {
-    resposta = "Treina timing e pratica no modo treino 👍";
-  } else if (texto.toLowerCase().includes("skill")) {
-    resposta = "Observa padrões dos jogadores e treina reação.";
-  } else {
-    resposta = "Ainda estou aprendendo isso, tenta perguntar diferente.";
-  }
+  let resposta = "Ainda estou aprendendo sobre isso 👍";
 
   mensagens.innerHTML += "<p><b>IA:</b> " + resposta + "</p>";
 
