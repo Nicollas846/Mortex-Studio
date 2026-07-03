2c9f5298b1cc01a472431de9ce31c52c94132ec1
Nicollas846 <gggerson1769@gmail.com>
2026-07-03 15:15:44 -0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
     }
 
     const completion = await groq.chat.completions.create({
-      model: "llama3-70b-versatile",
+     model: "llama-3.1-8b-instant",
       messages: [
         {
           role: "system",
